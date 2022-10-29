cc48628e982517d69a65e4810e44f5dc33740722
Marissa Loya <mloya1207@gmail.com>
2022-10-29 17:46:19 -0400

    renamed file

--- hh_db.py
+++ hh_db.py
@@ -1,105 +1,148 @@
-#!/usr/bin/env python3
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, DateTime, Float
+from sqlalchemy_utils import database_exists, create_database
 
-from flask_sqlalchemy import SQLAlchemy
+# Define the MariaDB engine using MariaDB Connector/Python
+# Don't forget to update user and password here 
+engine = sqlalchemy.create_engine("mysql://user:password@127.0.0.1:3306/HH")
+if not database_exists(engine.url):
+    create_database(engine.url)
 
-## this is a placeholder activation that needs to attach to a flask app later (in main.py)
-db = SQLAlchemy()
+Base = declarative_base()
 
-## XXX TODO: make sure every string value pulled from the dict in
-##      db.Model.metadata.tables matches the name of the actual table in the db!!
-##      Also make sure the value used in repr exists and is the right type!
+# Declarative mapping configurations
+class User(Base):
+    __tablename__ = 'USER'
+    UserID = Column(Integer, primary_key=True)
+    FirstName = Column(String(length=30), nullable=False)
+    LastName = Column(String(length=30), nullable=False)
+    Email = Column(String(length=50), nullable=False)
+    PhoneNumber = Column(String(length=12))
+    Password = Column(String(length=32), nullable=False)
+    IsAtRisk = Column(Boolean, default=False, nullable=False)
+    IsVolunteer = Column(Boolean, default=False, nullable=False)
+    IsRepresentative = Column(Boolean, default=False, nullable=False)
 
-class User(db.Model):
-    """
-    Any person (user, representative, etc)...
-    """
-    db.Model.metadata.tables['User']
-
-    def __repr__(self):
-        return repr(self.UserID)
-
-class Representative(db.Model):
-    """
-    User (rel) who represents an Organization
-    """
-    db.Model.metadata.tables['Representative']
-
-    def __repr__(self):
-        return repr(self.UserID)
-
-class Organization(db.Model):
-    """
-    Any group, optionally linked by any Representative or Locality
-    """
-    db.Model.metadata.tables['Organization']
+    def __init__(self, FirstName, LastName):
+        self.FirstName = FirstName
+        self.LastName = LastName
 
     def __repr__(self):
-        return repr(self.OrganizationID)
+        return "%s %s" %(self.FirstName, self.LastName) 
 
-class Locality(db.Model):
-    """
-    Physial location/address linked to an Organization or Program
-    """
-    db.Model.metadata.tables['Locality']
-
-    def __repr__(self):
-        return repr(self.LocalityID)
-
-class Program(db.Model):
-    """
-    Initiative (optionally sub'd to an Organization) linked to a Locality
-    """
-    db.Model.metadata.tables['Program']
+class Organization(Base):
+    __tablename__ = 'ORGANIZATION'
+    OrganizationID = Column(Integer, primary_key=True)
+    Name = Column(String(length=50), nullable=False)
+    HQAdress = Column(String(length=100))
+    PhoneNumber = Column(String(length=12))
+    Hours = Column(String(length=200))
+   
+    def __init__(self, OrganizationID, Name):
+        self.OrganizationID = OrganizationID
+        self.Name = Name
 
     def __repr__(self):
-        return repr(self.ProgramID)
+        return "%s %s" %(self.OrganizationID, self.Name) 
 
-class Status(db.Model):
-    """
-    Status of ranks and voting associated with a Page
-    """
-    db.Model.metadata.tables['Program']
-
-    def __repr__(self):
-        return repr(self.ProgramID)
-
-class Ratings(db.Model):
-    """
-    possibly defunct?
-    """
-    db.Model.metadata.tables['Ratings']
+class Representative(Base):
+    __tablename__ = 'REPRESENTATIVE'
+    UserID = Column(Integer, ForeignKey("USER.UserID"), primary_key=True)
+    OrganizationID = Column(Integer, ForeignKey("ORGANIZATION.OrganizationID"), primary_key=True)
+    VerificationStatus = Column(Boolean, default=False, nullable=False)
+   
+    def __init__(self, UserID, OrganizationID):
+        self.UserID = UserID
+        self.OrganizationID = OrganizationID
 
     def __repr__(self):
-        return repr(self.RatingID)
+        return "%s %s" %(self.UserID, self.OrganizationID) 
 
-class Forum(db.Model):
-    """
-    user-submitted posts attached to a Page
-    """
-    db.Model.metadata.tables['Forum']
-
-    def __repr__(self):
-        return repr(self.ForumID)
-
-class Page(db.Model):
-    """
-    aggregation of all elements to display for an Organization/Program/Locality
-    """
-    db.Model.metadata.tables['Page']
+class Program(Base):
+    __tablename__ = 'PROGRAM'
+    ProgramID = Column(Integer, primary_key=True)
+    Name = Column(String(length=50), nullable=False)
+    Description = Column(String(length=200))
+   
+    def __init__(self, ProgramID, Name):
+        self.ProgramID = ProgramID
+        self.Name = Name
 
     def __repr__(self):
-        return repr(self.PageID)
+        return "%s %s" %(self.ProgramID, self.Name) 
 
-
-class UsageMetrics(db.Model):
-    """
-    statistics derived from a Page
-    """
-    db.Model.metadata.tables['Usage_Metrics']
+class Locality(Base):
+    __tablename__ = 'LOCALITY'
+    LocalityID = Column(Integer, primary_key=True)
+    OrganizationID = Column(Integer, ForeignKey("ORGANIZATION.OrganizationID"))
+    ProgramID = Column(Integer, ForeignKey("PROGRAM.ProgramID"))
+    Address = Column(String(length=100), nullable=False)
+    PhoneNumber = Column(String(length=12), nullable=False)
+    Hours = Column(String(length=200))
+   
+    def __init__(self, LocalityID):
+        self.LocalityID = LocalityID
 
     def __repr__(self):
-        return repr(self.MetricID)
+        return "%s" %(self.LocalityID) 
 
+class Page(Base):
+    __tablename__ = 'PAGE'
+    PageID = Column(Integer, primary_key=True)
+    OrganizationID = Column(Integer, ForeignKey("ORGANIZATION.OrganizationID"))
+    ProgramID = Column(Integer, ForeignKey("PROGRAM.ProgramID"))
+    LocalityID = Column(Integer, ForeignKey("LOCALITY.LocalityID")) 
+    AcceptsWho = Column(String(length=100))
+    ProvidesTransportation = Column(Boolean)
+    LastUpdate = Column(DateTime)
+   
+    def __init__(self, PageID):
+        self.PageID = PageID
 
+    def __repr__(self):
+        return "%s" %(self.PageID) 
 
+class Forum(Base):
+    __tablename__ = 'FORUM'
+    PostID = Column(Integer, primary_key=True)
+    TimeStamp = Column(DateTime, nullable=False)
+    Comment = Column(String(length=200), nullable=False)
+    PageID = Column(Integer, ForeignKey("PAGE.PageID"), nullable=False)
+   
+    def __init__(self, PostID):
+        self.PostID = PostID
+
+    def __repr__(self):
+        return "%s" %(self.PostID) 
+
+class Vote(Base):
+    __tablename__ = 'VOTE'
+    VoteID = Column(Integer, primary_key=True)
+    Vote = Column(Boolean, nullable=False)
+    PageID = Column(Integer, ForeignKey("PAGE.PageID"), nullable=False)
+   
+    def __init__(self, VoteID):
+        self.VoteID = VoteID
+
+    def __repr__(self):
+        return "%s" %(self.VoteID) 
+
+class Usage_Metrics(Base):
+    __tablename__ = 'USAGE_METRICS'
+    MetricID = Column(Integer, primary_key=True)
+    PageID = Column(Integer, ForeignKey("PAGE.PageID"), nullable=False) 
+    AvgTimeSpent = Column(Float(precision=10), nullable=False)
+    NumVisits = Column(Integer, nullable=False)
+    NumForums = Column(Integer, nullable=False)
+    NumUpVotes = Column(Integer, nullable=False)
+    NumDownVotes = Column(Integer, nullable=False)
+   
+    def __init__(self, MetricID):
+        self.MetricID = MetricID
+
+    def __repr__(self):
+        return "%s" %(self.MetricID) 
+
+Base.metadata.create_all(engine)
 
