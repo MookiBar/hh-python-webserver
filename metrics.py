ca978a7b8ee2c0633939f645c497aff7d9ef2aba
Marissa Loya <mloya1207@gmail.com>
2022-11-29 16:08:26 -0500

    Metric Collection: Vote count functions

--- metrics.py
+++ metrics.py
@@ -1,7 +1,7 @@
 import hh_db as hh
 
 """
-countForum('PageID') to count number of rows in a table matching a certain PageID
+countForum('PageID') to count number of rows in Forum table matching a certain PageID
 
 Metrics Collection: Used to obtain NumForumPosts
 
@@ -13,4 +13,32 @@ def countForum(pageID):
         numForums = session.query(hh.Forum).filter(hh.Forum.PageID==pageID).count()
         return numForums
 
-        
+"""
+countUpVotes('Vote_table_name', 'PageID') to count number of up votes in a Vote table matching a certain PageID
+
+Metrics Collection: Used to obtain NumUpVotesClean/NumUpVotesSafe/NumUpVotesResponsive
+
+example: metrics.countUpVotes( hh_db.Safe_Vote, 1 )
+example: metrics.countUpVotes( hh_db.Clean_Vote, 1 )
+example: metrics.countUpVotes( hh_db.Responsive_Vote, 1 )
+returns: Up Votes on PageID==1
+"""
+def countUpVotes(category, pageID):
+    with hh.Session.begin() as session:
+        numUpVotes = session.query(category).filter(category.PageID==pageID).filter(category.Vote==True).count()
+        return numUpVotes
+
+"""
+countDownVotes('Vote_table_name', 'PageID') to count number of down votes in a Vote table matching a certain PageID
+
+Metrics Collection: Used to obtain NumDownVotesClean/NumDownVotesSafe/NumDownVotesResponsive
+
+example: metrics.countDownVotes( hh_db.Safe_Vote, 1 )
+example: metrics.countDownVotes( hh_db.Clean_Vote, 1 )
+example: metrics.countDownVotes( hh_db.Responsive_Vote, 1 )
+returns: Down Votes on PageID==1
+"""
+def countDownVotes(category, pageID):
+    with hh.Session.begin() as session:
+        numDownVotes = session.query(category).filter(category.PageID==pageID).filter(category.Vote==False).count()
+        return numDownVotes
