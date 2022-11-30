c289ba9132d4330dc99930f8f4540831e8c2bd32
Marissa Loya <mloya1207@gmail.com>
2022-11-29 22:07:21 -0500

    updateMetric and addMetric functions

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import hh_db as hh
+from sqlalchemy import update
 
 """
 params: pageID - PageID for which we are obtaining metric
@@ -71,3 +72,48 @@ def avgTimeSpent():
     ##TODO: implement to calculate avgTimeSpent on page for total page visit
     return 0
 
+"""
+params: pageID - PageID for which we are obtaining metric
+
+Metrics Collection: Used to obtain AvgTimeSpent on a certain PageID by all users who visited the page
+
+return: double timeSpent
+"""
+def updateMetric(pageID, avgTime, visitsAtRisk, visitsVolunteer, visitsRep, visitsOther, numForums, upVoteClean, downVoteClean, upVoteResponsive, downVoteResponsive, upVoteSafe, downVoteSafe):
+    with hh.Session.begin() as session:
+        stmt = (
+            update(hh.Usage_Metrics).
+            where(hh.Usage_Metrics.PageID == pageID).
+            values(AvgTimeSpent = avgTime, NumVisitsAtRisk = visitsAtRisk, NumVisitsVolunteer = visitsVolunteer, NumVisitsRepresentative = visitsRep, NumVisitsOther = visitsOther, NumForumPosts = numForums, NumUpVotesClean = upVoteClean, NumDownVotesClean = downVoteClean, NumUpVotesResponsive = upVoteResponsive, NumDownVotesResponsive = downVoteResponsive, NumUpVotesSafe = upVoteSafe, NumDownVotesSafe = downVoteSafe)
+        )
+        session.execute(stmt)
+        session.commit()
+
+"""
+params: pageID - PageID for which we are adding/updating metric
+
+Metrics Collection: Adds/Updates the Usage Metrics for a certain page
+
+example: metrics.addMetric( 1 )
+"""
+def addMetric(pageID):
+    with hh.Session.begin() as session:
+        avgTime = avgTimeSpent()
+        visitsAtRisk = countVisits()
+        visitsVolunteer = countVisits()
+        visitsRep = countVisits()
+        visitsOther = countVisits()
+        numForums = countForum(pageID)
+        upVoteClean = countUpVotes(hh.Clean_Vote, pageID)
+        downVoteClean = countDownVotes(hh.Clean_Vote, pageID)
+        upVoteResponsive = countUpVotes(hh.Responsive_Vote, pageID)
+        downVoteResponsive = countDownVotes(hh.Responsive_Vote, pageID)
+        upVoteSafe = countUpVotes(hh.Safe_Vote, pageID)
+        downVoteSafe = countDownVotes(hh.Safe_Vote, pageID)
+
+        metric = session.query(hh.Usage_Metrics).filter(hh.Usage_Metrics.PageID == pageID).count()
+
+        if metric == 0:
+            hh.add_db_object( hh.Usage_Metrics(pageID, avgTime, visitsAtRisk, visitsVolunteer, visitsRep, visitsOther, numForums, upVoteClean, downVoteClean, upVoteResponsive, downVoteResponsive, upVoteSafe, downVoteSafe) )
+        else:
+            updateMetric(pageID, avgTime, visitsAtRisk, visitsVolunteer, visitsRep, visitsOther, numForums, upVoteClean, downVoteClean, upVoteResponsive, downVoteResponsive, upVoteSafe, downVoteSafe)
