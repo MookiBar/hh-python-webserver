a670ea6162e7cd78eaaadc35ffa1a1c6f5b96bfd
Marissa Loya <mloya1207@gmail.com>
2022-11-30 11:39:05 -0500

    Corrected class documentation

--- metrics.py
+++ metrics.py
@@ -1,85 +1,75 @@
 import hh_db as hh
 from sqlalchemy import update
 
-"""
-params: pageID - PageID for which we are obtaining metric
-
-Metrics Collection: Used to obtain NumForumPosts for a certain PageID
-
-example: metrics.countForum( 1 )
-
-return: int numForums
-"""
 def countForum(pageID):
+    """
+    params: pageID - PageID for which we are obtaining metric
+
+    Metrics Collection: Used to obtain NumForumPosts for a certain PageID
+
+    return: int numForums
+    """
     with hh.Session.begin() as session:
         numForums = session.query(hh.Forum).filter(hh.Forum.PageID==pageID).count()
         return numForums
 
-"""
-params: category - Vote category i.e. safe/clean/responsive 
-        pageID - PageID for which we are obtaining metric
-
-Metrics Collection: Used to obtain NumUpVotesClean/NumUpVotesSafe/NumUpVotesResponsive for a certain PageID
-
-example: metrics.countUpVotes( hh_db.Safe_Vote, 1 )
-example: metrics.countUpVotes( hh_db.Clean_Vote, 1 )
-example: metrics.countUpVotes( hh_db.Responsive_Vote, 1 )
-
-return: int numUpVotes
-"""
 def countUpVotes(category, pageID):
+    """
+    params: category - Vote category i.e. safe/clean/responsive 
+            pageID - PageID for which we are obtaining metric
+
+    Metrics Collection: Used to obtain NumUpVotesClean/NumUpVotesSafe/NumUpVotesResponsive for a certain PageID
+
+    return: int numUpVotes
+    """
     with hh.Session.begin() as session:
         numUpVotes = session.query(category).filter(category.PageID==pageID).filter(category.Vote==True).count()
         return numUpVotes
 
-"""
-params: category - Vote category i.e. safe/clean/responsive 
-        pageID - PageID for which we are obtaining metric
-
-Metrics Collection: Used to obtain NumDownVotesClean/NumDownVotesSafe/NumDownVotesResponsive for a certain PageID
-
-example: metrics.countDownVotes( hh_db.Safe_Vote, 1 )
-example: metrics.countDownVotes( hh_db.Clean_Vote, 1 )
-example: metrics.countDownVotes( hh_db.Responsive_Vote, 1 )
-
-return: int numDownVotes
-"""
 def countDownVotes(category, pageID):
+    """
+    params: category - Vote category i.e. safe/clean/responsive 
+            pageID - PageID for which we are obtaining metric
+
+    Metrics Collection: Used to obtain NumDownVotesClean/NumDownVotesSafe/NumDownVotesResponsive for a certain PageID
+
+    return: int numDownVotes
+    """
     with hh.Session.begin() as session:
         numDownVotes = session.query(category).filter(category.PageID==pageID).filter(category.Vote==False).count()
         return numDownVotes
 
-"""
-params: userType - AtRisk/Volunteer/Representative
-        pageID - PageID for which we are obtaining metric
-
-Metrics Collection: Used to obtain NumVisits for a specific userType on a certain PageID
-
-return: int numVisits
-"""
 def countVisits():
+    """
+    params: userType - AtRisk/Volunteer/Representative
+            pageID - PageID for which we are obtaining metric
+
+    Metrics Collection: Used to obtain NumVisits for a specific userType on a certain PageID
+
+    return: int numVisits
+    """
     ##TODO: implement to count number of times page is accessed by a specific userType
     return 0
 
-"""
-params: pageID - PageID for which we are obtaining metric
-
-Metrics Collection: Used to obtain AvgTimeSpent on a certain PageID by all users who visited the page
-
-return: double timeSpent
-"""
 def avgTimeSpent():
+    """
+    params: pageID - PageID for which we are obtaining metric
+
+    Metrics Collection: Used to obtain AvgTimeSpent on a certain PageID by all users who visited the page
+
+    return: double timeSpent
+    """
     ##TODO: implement to calculate avgTimeSpent on page for total page visit
     return 0
 
-"""
-params: pageID - PageID for which we are obtaining metric
-
-Metrics Collection: Used to obtain AvgTimeSpent on a certain PageID by all users who visited the page
-
-return: double timeSpent
-"""
 def updateMetric(pageID, avgTime, visitsAtRisk, visitsVolunteer, visitsRep, visitsOther, numForums, upVoteClean, downVoteClean, upVoteResponsive, downVoteResponsive, upVoteSafe, downVoteSafe):
+    """
+    params: pageID - PageID for which we are obtaining metric
+
+    Metrics Collection: Used to obtain AvgTimeSpent on a certain PageID by all users who visited the page
+
+    return: double timeSpent
+    """
     with hh.Session.begin() as session:
         stmt = (
             update(hh.Usage_Metrics).
@@ -89,14 +79,14 @@ def updateMetric(pageID, avgTime, visitsAtRisk, visitsVolunteer, visitsRep, visi
         session.execute(stmt)
         session.commit()
 
-"""
-params: pageID - PageID for which we are adding/updating metric
-
-Metrics Collection: Adds/Updates the Usage Metrics for a certain page
-
-example: metrics.addMetric( 1 )
-"""
 def addMetric(pageID):
+    """
+    params: pageID - PageID for which we are adding/updating metric
+
+    Metrics Collection: Adds/Updates the Usage Metrics for a certain page
+
+    example: metrics.addMetric( 1 )
+    """
     with hh.Session.begin() as session:
         avgTime = avgTimeSpent()
         visitsAtRisk = countVisits()
