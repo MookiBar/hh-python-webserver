22b8dd84dc01e249faf43f160039c0bcfe1427cd
Marissa Loya <mloya1207@gmail.com>
2022-11-29 19:17:47 -0500

    Placeholders for countVisits & avgTimeSpent functions

--- metrics.py
+++ metrics.py
@@ -1,12 +1,13 @@
 import hh_db as hh
 
 """
-countForum('PageID') to count number of rows in Forum table matching a certain PageID
+params: pageID - PageID for which we are obtaining metric
 
-Metrics Collection: Used to obtain NumForumPosts
+Metrics Collection: Used to obtain NumForumPosts for a certain PageID
 
 example: metrics.countForum( 1 )
-returns: Forum posts on PageID==1
+
+return: int numForums
 """
 def countForum(pageID):
     with hh.Session.begin() as session:
@@ -14,14 +15,16 @@ def countForum(pageID):
         return numForums
 
 """
-countUpVotes('Vote_table_name', 'PageID') to count number of up votes in a Vote table matching a certain PageID
+params: category - Vote category i.e. safe/clean/responsive 
+        pageID - PageID for which we are obtaining metric
 
-Metrics Collection: Used to obtain NumUpVotesClean/NumUpVotesSafe/NumUpVotesResponsive
+Metrics Collection: Used to obtain NumUpVotesClean/NumUpVotesSafe/NumUpVotesResponsive for a certain PageID
 
 example: metrics.countUpVotes( hh_db.Safe_Vote, 1 )
 example: metrics.countUpVotes( hh_db.Clean_Vote, 1 )
 example: metrics.countUpVotes( hh_db.Responsive_Vote, 1 )
-returns: Up Votes on PageID==1
+
+return: int numUpVotes
 """
 def countUpVotes(category, pageID):
     with hh.Session.begin() as session:
@@ -29,16 +32,42 @@ def countUpVotes(category, pageID):
         return numUpVotes
 
 """
-countDownVotes('Vote_table_name', 'PageID') to count number of down votes in a Vote table matching a certain PageID
+params: category - Vote category i.e. safe/clean/responsive 
+        pageID - PageID for which we are obtaining metric
 
-Metrics Collection: Used to obtain NumDownVotesClean/NumDownVotesSafe/NumDownVotesResponsive
+Metrics Collection: Used to obtain NumDownVotesClean/NumDownVotesSafe/NumDownVotesResponsive for a certain PageID
 
 example: metrics.countDownVotes( hh_db.Safe_Vote, 1 )
 example: metrics.countDownVotes( hh_db.Clean_Vote, 1 )
 example: metrics.countDownVotes( hh_db.Responsive_Vote, 1 )
-returns: Down Votes on PageID==1
+
+return: int numDownVotes
 """
 def countDownVotes(category, pageID):
     with hh.Session.begin() as session:
         numDownVotes = session.query(category).filter(category.PageID==pageID).filter(category.Vote==False).count()
         return numDownVotes
+
+"""
+params: userType - AtRisk/Volunteer/Representative
+        pageID - PageID for which we are obtaining metric
+
+Metrics Collection: Used to obtain NumVisits for a specific userType on a certain PageID
+
+return: int numVisits
+"""
+def countVisits():
+    ##TODO: implement to count number of times page is accessed by a specific userType
+    return 0
+
+"""
+params: pageID - PageID for which we are obtaining metric
+
+Metrics Collection: Used to obtain AvgTimeSpent on a certain PageID by all users who visited the page
+
+return: double timeSpent
+"""
+def avgTimeSpent():
+    ##TODO: implement to calculate avgTimeSpent on page for total page visit
+    return 0
+
